5b577b69b382e8edcae91c6073749713d1ce3b2c
Sanchklyc <halitenes.kalayci@gmail.com>
2021-05-06 15:47:24 +0300

    .

--- eCommerceSimulation/src/eCommerceSimulation/Main.java
+++ eCommerceSimulation/src/eCommerceSimulation/Main.java
@@ -16,28 +16,28 @@ public class Main {
 		
 		UserService userService = new UserManager(new InMemoryUserDao());
 		AuthService authService = new AuthManager(userService, new UserValidationManager(), new EmailManager());
-		authService.register(1, "halitenes.kalayci@gmail.com", "123halit1234", "Halit Enes", "Kalaycý"); // Başarılı
-		authService.register(2, "bubirepostadeðil..", "þifrevalid", "Halit Enes", "Kalaycý"); // Başarısız eposta invalid
-		authService.register(3, "bubireposta@gmail.com", "s", "Halit Enes", "Kalaycý"); // Başarısız şifre invalid
+		authService.register(1, "halitenes.kalayci@gmail.com", "123halit1234", "Halit Enes", "Kalaycı"); // Başarılı
+		authService.register(2, "bubirepostadeðil..", "þifrevalid", "Halit Enes", "Kalaycı"); // Başarısız eposta invalid
+		authService.register(3, "bubireposta@gmail.com", "s", "Halit Enes", "Kalaycı"); // Başarısız şifre invalid
 		authService.register(4, "bubireposta@gmail.com", "s", "H", "K"); // Başarısız ad-soyad invalid
-		authService.register(5, "halitenes.kalayci@gmail.com", "123halit1234", "Halit Enes", "Kalaycý"); // Başarısız e-posta mevcut
+		authService.register(5, "halitenes.kalayci@gmail.com", "123halit1234", "Halit Enes", "Kalaycı"); // Başarısız e-posta mevcut
 
-<<<<<<< HEAD
-		authService.login("halitenes.kalayci@gmail.com", "123halit1234"); // Ba�ar�s�z �ye do�rulama yapmam��.
-		userService.verifyUser(1); // Kullan�c�y� do�rulayal�m.
-		authService.login("halitenes.kalayci@gmail.com", "123halit123"); // Ba�ar�s�z �ifre yanl��.
-		authService.login("halitenes.kalayci@gmail.com", "123halit1234"); // Ba�ar�l�.
-		authService.login("", ""); // Ba�ar�s�z e-posta parola zorunlu
-=======
+
+		authService.login("halitenes.kalayci@gmail.com", "123halit1234"); // Başarşsşz üye doşrulama yapmamşş.
+		userService.verifyUser(1); // Kullanşcşyş doşrulayalşm.
+		authService.login("halitenes.kalayci@gmail.com", "123halit123"); // Başarısız şifre yanlış.
+		authService.login("halitenes.kalayci@gmail.com", "123halit1234"); // Başarşlş.
+		authService.login("", ""); // Başarşsşz e-posta parola zorunlu
+
 		authService.login("halitenes.kalayci@gmail.com", "123halit1234"); // Başarısız üye doğrulama yapmamış.
-		userService.verifyUser(userService.getByEmail("halitenes.kalayci@gmail.com")); // Kullanıcıyı doğrulayalım.
+		userService.verifyUser(1); // Kullanıcıyı doğrulayalım.
 		authService.login("halitenes.kalayci@gmail.com", "123halit123"); // Başarısız şifre yanlış.
 		authService.login("halitenes.kalayci@gmail.com", "123halit1234"); // Başarılı.
 		authService.login("", ""); // Başarısız e-posta parola zorunlu
->>>>>>> edba22a9adbd4adaca29d87340aacf41a977443b
+
 		
 		AuthService googleAuthService = new GoogleAuthManagerAdapter();
-		googleAuthService.register(6, "klyyc7@gmail.com", "123halit1234", "Halit Enes", "Kalaycý");
+		googleAuthService.register(6, "klyyc7@gmail.com", "123halit1234", "Halit Enes", "Kalaycı");
 		googleAuthService.login("klyyc7@gmail.com", "123halit1234");
 		
 		
